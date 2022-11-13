d2c69124d7cec01111fa84d270b86f8e05de004c
pomorenor <pomorenor@unal.edu.co>
2022-11-12 20:09:22 -0500

    Se añade función para encontrar los vecinos más cercanos

--- percolib.py
+++ percolib.py
@@ -13,7 +13,7 @@ def fill_lattice(A,L,M):
             random_nums.remove(A[i,j])
     return A
 
-#RanMtz genera una matriz con tamaño dado, con 0 y 1 y recibe el tamaño 
+#RanMtz genera una matriz con tamaño dado, con 0 y 1 y recibe el tamaño
 #la probabilidad de que una celda tenga un 1
 def RanMtz(p, size):
     M=np.zeros((size[0],size[1]))
@@ -24,7 +24,7 @@ def RanMtz(p, size):
                 M[ii,jj]=1
     return M
 
-#HK recibe una matriz (en principio de 0 y 1) y devuelve una matriz de 
+#HK recibe una matriz (en principio de 0 y 1) y devuelve una matriz de
 #enteros donde cada cluster está clasificado
 
 def HK(Ma):
@@ -32,10 +32,10 @@ def HK(Ma):
     N=1
     for ii in range(len(M)):
         for jj in range(len(M[0])):
-            if M[ii,jj]!=0:  
+            if M[ii,jj]!=0:
                 if ii==0 and jj==0:
                     M[ii,jj]=N
-                    N+=1  
+                    N+=1
                 elif ii==0:
                     if M[ii,jj-1]!=0:
                         M[ii,jj]=M[ii,jj-1]
@@ -54,19 +54,19 @@ def HK(Ma):
                     elif M[ii,jj-1]!=0:
                         M[ii,jj]=M[ii,jj-1]
                     else:
-                        M[ii,jj]=N   
+                        M[ii,jj]=N
                         N+=1
     for ii in range(len(M)):
-        for jj in range(len(M[0])):                     
+        for jj in range(len(M[0])):
             if M[ii,jj-1]!=0 and M[ii,jj]!=0:
                 if M[ii,jj-1]!=M[ii,jj] and jj!=0:
                     V=M[ii,jj-1]
                     U=M[ii,jj]
                     for i in range(len(M)):
-                        for j in range(len(M[0])):  
+                        for j in range(len(M[0])):
                             if M[i,j]==V:
                                 M[i,j]=U
-                                      
+
     return M
 
 #CL recibe una matriz (de 0 y 1 o ya clasificada) y cuenta sus clusters
@@ -78,18 +78,18 @@ def CL(Ma):
     return N
 
 #Percolador recibe una matriz (de 0 y 1 o ya clasificada) y devuelve
-#una matriz con los clusters percolantes 
+#una matriz con los clusters percolantes
 def Percolador(Ma):
     M=HK(Ma)
     M0=set(M[0])
     MF=set(M[len(M)-1])
     percolantes= list(M0 & MF -{0})
     Z=np.zeros((len(M),len(M[0])))
-    
+
     if len(percolantes)!=0:
         for V in percolantes:
             for ii in range(len(M)):
-                for jj in range(len(M[0])):                 
+                for jj in range(len(M[0])):
                     if M[ii,jj]==V:
                         Z[ii,jj]+=M[ii,jj]
     return Z
@@ -105,3 +105,21 @@ if __name__ == '__main__':
     print("Número de Clusters:",CL(M))
     print("Número de Clusters Percolantes:",CL(Percolador(M)))
     plt.show()
+
+
+### Función que retorna los indices y los valores de los vecinos de una celda del cluster
+
+def closest_neighbours(A,i,j, L, M):
+
+    pn = [[i-1,j], [i+1,j], [i,j-1], [i,j+1]]
+
+    for i, t in enumerate(pn):
+        if t[0] < 0 or t[1] < 0 or t[0] >= L or t[1] >= M:
+            pn[i] = None
+    indices = [c for c in pn if c is not None]
+
+    neighbours = []
+    for j in indices:
+        neighbours.append(A[j[0]][j[1]])
+
+    return indices, neighbours
